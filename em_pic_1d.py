cd8c8157d86b81b28a0fa1adc4b0c416a52e2e36
Remi Lehe <rlehe@lbl.gov>
2016-05-26 15:03:51 -0700

    Class syntax for em_pic_1d.py

--- em_pic_1d.py
+++ em_pic_1d.py
@@ -18,106 +18,118 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.constants import c # Numerical value of the speed of light
 
-# Numerical parameters of the simulation
-# --------------------------------------
-Lz = 100.        # Total length of the physical domain
-Nz = 100         # Number of grid points
-dz = Lz/Nz       # Spacing between gridpoints along the z axis
-dt = dz/c  # Timestep of the simulation
+class EM1DSolver(object):
 
-# Initialize the field arrays
-# ---------------------------
-n = 0   # Iteration number
-Ex = np.zeros( Nz+2 )    # At iteration n, Ex[k] represents the field
-                         # at time n*dt and position (k + 1/2)*dz
-By = np.zeros( Nz+2 )    # At iteration n, By[k] represents the field
-                         # at time (n - 1/2)*dz and position k*dz
-# The first and last element of Ex and By are ghost cell: they simply duplicate
-# the last-but-one and second element respectively (periodic boundaries)
+    def __init__(self, Nz=100, Lz=100., dtcoef=1.):
+        """
+        Initialize the EM1DSolver object
 
-# Initialize the field values with an oscillating function
-# --------------------------------------------------------
-k = 2*np.pi/5. # Wavevector of the pulse
-L = 20.  # Length of the pulse
-def pulse_shape(z, t):
-    """Return the profile of the pulse: gaussian envelope + oscillations"""
-    return( np.cos(k*(z-c*t)) * np.exp(-(z-c*t-Lz/2)**2/L**2) )
-z = np.arange(1,Nz+1)*dz
-Ex[1:-1] = pulse_shape( z + 0.5*dz, 0 )
-By[1:-1] = 1./c * pulse_shape( z, -0.5*dt )
+        Parameters
+        ----------
+        Nz: int
+           Number of grid points
 
-# Definition of the step and plot_fields functions
-# ------------------------------------------------
-def step(N_steps):
-    """
-    Perform `N_steps` iterations of the field update
-    """
-    global n, Ex, By
-    print( 'Performing %d iterations' %N_steps )
-    
-    # Loop over the timesteps
-    for i_step in range(N_steps):
+        Lz: real
+           Total length of the physical domain
 
-        # Update the By field from time (n-1/2)*dt to time (n+1/2)*dt
-        for k in range(1,Nz+1):
-            # ASSIGNMENT: Replace "+ 0" by the appropriate expression
-            By[k] = By[k] + 0
-        # Apply periodic boundary conditions (do not modify)
-        By[0] = By[Nz-1]
-        By[Nz] = By[1]
+        dtcoef: real
+           The timestep will be:
+           dt = dtcoef*dz/c  with dz = Lz/Nz
+        """
+        self.Nz = Nz
+        self.dz = Lz/Nz  # Spacing between gridpoints along the z axis
+        self.dt = dtcoef*self.dz/c  # Timestep of the simulation
+        self.Lz = Lz
+
+        self.n = 0  # Iteration number
+        self.z = np.arange(1,Nz+1)*self.dz  # Position of the grid points
+        self.Ex = np.zeros( Nz+2 )  # At iteration n, Ex[k] represents the 
+                                # field at time n*dt and position (k + 1/2)*dz
+        self.By = np.zeros( Nz+2 )  # At iteration n, By[k] represents the
+                                # field at time (n - 1/2)*dz and position k*dz
+        # The first and last element of Ex and By are ghost cell: they simply
+        # duplicate the last-but-one and second element respectively
+        # (periodic boundaries)
+
+        # Initialize the field values with an oscillating function
+        k = 2*np.pi/5. # Wavevector of the pulse
+        L = 20.  # Length of the pulse
+        def pulse_shape(z, t):
+            return( np.cos(k*(z-c*t)) * np.exp(-(z-c*t-Lz/2)**2/L**2) )
+
+        self.Ex[1:-1] = pulse_shape( self.z + 0.5*self.dz, 0 )
+        self.By[1:-1] = 1./c * pulse_shape( self.z, -0.5*self.dt )
+
+
+    def step(self, N_steps):
+        """
+        Perform `N_steps` iterations of the field update
+        """
+        print( 'Performing %d iterations' %N_steps )
         
-        # Update the Ex field from time n*dt to time (n+1)*dt
-        # Loop over the gridpoints
-        for k in range(1,Nz+1):
-            # ASSIGNMENT: Replace "+ 0" by the appropriate expression
-            Ex[k] = Ex[k] + 0
-        # Apply periodic boundary conditions (do not modify)
-        Ex[0] = Ex[Nz-1]
-        Ex[Nz] = Ex[1]
+        # Loop over the timesteps
+        for i_step in range(N_steps):
 
-        # Increment the iteration number
-        n = n+1
-
-def plot_fields( save_figure=False ):
-    """
-    Plot the Ex and By field using matplotlib
-    If save_figure is True, the plots are saved as PNG files,
-    in a folder named `diagnostics`
-    """
-    print( 'Plotting the fields at iteration %d' %n )
+            # Update the By field from time (n-1/2)*dt to time (n+1/2)*dt
+            for k in range(1,self.Nz+1):
+                # ASSIGNMENT: Replace "+ 0" by the appropriate expression
+                self.By[k] = self.By[k] + 0
+            # Apply periodic boundary conditions (do not modify)
+            self.By[0] = self.By[self.Nz-1]
+            self.By[self.Nz] = self.By[1]
+            
+            # Update the Ex field from time n*dt to time (n+1)*dt
+            # Loop over the gridpoints
+            for k in range(1,self.Nz+1):
+                # ASSIGNMENT: Replace "+ 0" by the appropriate expression
+                self.Ex[k] = self.Ex[k] + 0
+            # Apply periodic boundary conditions (do not modify)
+            self.Ex[0] = self.Ex[self.Nz-1]
+            self.Ex[self.Nz] = self.Ex[1]
     
-    plt.clf()
-    plt.suptitle('Fields at iteration %d' %n)
-    # Plot of Ex
-    plt.subplot(211)
-    plt.plot( z+0.5*dz, Ex[1:-1], 'o-' )
-    plt.ylim(-1.1, 1.1)
-    plt.xlim(0, Lz)
-    plt.ylabel('$E_x^n$')
-    plt.xlabel('z')
-    # Plot of By
-    plt.subplot(212)
-    plt.plot( z, By[1:-1], 'o-' )
-    plt.ylim(-1.1/c, 1.1/c)
-    plt.xlim(0, Lz)
-    plt.ylabel('$B_y^{n-1/2}$')
-    plt.xlabel('z')
+            # Increment the iteration number
+            self.n = self.n+1
     
-    if save_figure is True:
-        # Check that the diagnostics folder exists
-        if os.path.exists('diagnostics') is False:
-            os.mkdir('diagnostics')
-        plt.savefig( "diagnostics/iteration%03d.png" %n) 
+    def plot_fields( self, save_figure=False ):
+        """
+        Plot the Ex and By field using matplotlib
+        If save_figure is True, the plots are saved as PNG files,
+        in a folder named `diagnostics`
+        """
+        print( 'Plotting the fields at iteration %d' %self.n )
 
+        plt.clf()
+        plt.suptitle('Fields at iteration %d' %self.n)
+        # Plot of Ex
+        plt.subplot(211)
+        plt.plot( self.z+0.5*self.dz, self.Ex[1:-1], 'o-' )
+        plt.ylim(-1.1, 1.1)
+        plt.xlim(0, self.Lz)
+        plt.ylabel('$E_x^n$')
+        plt.xlabel('z')
+        # Plot of By
+        plt.subplot(212)
+        plt.plot( self.z, self.By[1:-1], 'o-' )
+        plt.ylim(-1.1/c, 1.1/c)
+        plt.xlim(0, self.Lz)
+        plt.ylabel('$B_y^{n-1/2}$')
+        plt.xlabel('z')
+
+        if save_figure is True:
+            # Check that the diagnostics folder exists
+            if os.path.exists('diagnostics') is False:
+                os.mkdir('diagnostics')
+            plt.savefig( "diagnostics/iteration%03d.png" %self.n)
 
 if __name__ == '__main__':
-
+    
     # Remove the diagnostics folder if it exists
     if os.path.exists('diagnostics') is True:
         shutil.rmtree('diagnostics')
-
+    
     # Run Nz iterations (in 10 batches, with plotting inbetween)
-    for i in range(10):
-        plot_fields( save_figure=True )
-        step(Nz/10)
-    plot_fields( save_figure=True )
+    em = EM1DSolver( )
+    for i in range( 10 ):
+        em.plot_fields( save_figure=True )
+        em.step( 10 )
+    em.plot_fields( save_figure=True )
